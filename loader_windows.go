c84c95c911104cb3ae34bfb55c7c27141d18e2fb
John Chadwick <john@jchw.io>
2020-12-30 18:08:09 -0800

    Add module cache.
    This should allow linking multiple in-memory libraries together. Please note that such functionality is extremely experimental :)
    
    Closes #1.

--- loader_windows.go
+++ loader_windows.go
@@ -5,12 +5,28 @@ import (
 	"github.com/jchv/go-winloader/internal/winloader"
 )
 
+var native = winloader.Loader{}
+
+var cache = memloader.NewCache(native)
+
 var ldr = memloader.New(memloader.Options{
-	Next:    winloader.Loader{},
+	Next:    cache,
 	Machine: winloader.NativeMachine{},
 })
+
+// LoadFromFile loads a Windows module from file using the native Windows loader.
+func LoadFromFile(name string) (Module, error) {
+	return native.Load(name)
+}
 
 // LoadFromMemory loads a Windows module from memory.
 func LoadFromMemory(data []byte) (Module, error) {
 	return ldr.LoadMem(data)
+}
+
+// AddToCache adds a module to the loader cache, allowing in-memory libraries
+// to link to it. Note that modules in the cache must exist in the same
+// address space.
+func AddToCache(name string, module Module) error {
+	return cache.Add(name, module)
 }
